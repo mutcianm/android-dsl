54e193f9eab695786c1ef70fd459a9d4598b843f
dedoz <d3d0zz@gmail.com>
2013-11-19 15:25:46 +0400

    code cleanup
    - remove useless comments
    - typeFilter() uses 'when' instead of multiple if's
    - remove unreachable code from typeStr()
    - typeStr() generates optionally nullable types
    - remove StringBuffer preallocation
    - replace map with forEach when result is discarded
    - add kotlin-style map access
    - fix typos

--- src/com/adsl/Generator.kt
+++ src/com/adsl/Generator.kt
@@ -17,49 +17,45 @@ import java.io.BufferedWriter
 import java.io.OutputStreamWriter
 import java.util.TreeMap
 
-/**
- * Created with IntelliJ IDEA.
- * User: miha
- * Date: 11/16/13
- * Time: 11:30 PM
- */
-
 fun typeFilter(str: String): String {
-    if(str equals "java.lang.CharSequence") return "jet.CharSequence"
-    if(str equals "java.lang.String") return "jet.String"
-    if(str equals "java.lang.Integer") return "jet.Int"
-    if(str equals "java.lang.Object") return "jet.Any"
-    return str
+    when (str) {
+        "java.lang.CharSequence" -> return "jet.CharSequence"
+        "java.lang.String" -> return "jet.String"
+        "java.lang.Integer" -> return "jet.Int"
+        "java.lang.Object" -> return "jet.Any"
+        else -> return str
+    }
 }
 
-fun typeStr(_type: Type?): String {
-    when (_type?.getSort()) {
-        Type.BOOLEAN -> return "Boolean"
-        Type.INT -> return "Int"
-        Type.FLOAT -> return "Float"
-        Type.DOUBLE -> return "Double"
-        Type.LONG -> return "Long"
-        Type.ARRAY -> {
-            when (_type!!.getElementType()!!.getSort()) {
-                Type.INT -> return "IntArray?"
-                Type.FLOAT -> return "FloatArray?"
-                Type.DOUBLE -> return "DoubleArray?"
-                Type.LONG -> return "LongArray?"
-                null -> return "INVALID"
-                else -> {
-                    return "Array<" + typeFilter(typeStr(_type!!.getElementType())) + ">?"
+fun cleanInternalName(name: String): String {
+    return name.replace('/', '.').replace('$', '.')
+}
+
+fun typeStr(_type: Type?, nullable: Boolean = true): String {
+    if(_type != null) {
+        when (_type.getSort()) {
+            Type.BOOLEAN -> return "Boolean"
+            Type.INT -> return "Int"
+            Type.FLOAT -> return "Float"
+            Type.DOUBLE -> return "Double"
+            Type.LONG -> return "Long"
+            Type.ARRAY -> {
+                when (_type.getElementType()?.getSort()) {
+                    Type.INT -> return "IntArray?"
+                    Type.FLOAT -> return "FloatArray?"
+                    Type.DOUBLE -> return "DoubleArray?"
+                    Type.LONG -> return "LongArray?"
+                    else -> {
+                        return "Array<" + typeFilter(typeStr(_type.getElementType(), nullable = false)) + ">?"
+                    }
                 }
             }
-        }
-        null -> return "Any"
-        else -> {
-            val tmp = _type.toString()!!.replace('/', '.').replace('$', '.').substring(1)
-            if(tmp.isEmpty()) {
-                return "WTF"
+            else -> {
+                return typeFilter(cleanInternalName(_type.getInternalName()!!)) + if(nullable) "?" else ""
             }
-            return typeFilter(tmp.substring(0, tmp.length - 1)) + '?'
         }
-    }
+    } else
+        throw RuntimeException("Type can't be null(this sholdn't happen)")
 }
 
 
@@ -93,15 +89,15 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
     val ps = BufferedWriter(OutputStreamWriter(out))
     val settings = GeneratorSettings()
 
-    val propsCache = StringBuffer(2048)
-    val containerCache = StringBuffer(2048)
+    val propsCache = StringBuffer()
+    val containerCache = StringBuffer()
 
-    class PropData(val classname: String, val propName: String, var propType: Type?, var getter: String?, var setter: String?, var arg: Type?)
+    class PropData(val className: String, val propName: String, var propType: Type?, var getter: String?, var setter: String?, var arg: Type?)
     val propMap = TreeMap<String, PropData>()
 
     public fun run() {
         genHeader()
-        extractClasses(jarPath, packageName) map { processClass(it) }
+        extractClasses(jarPath, packageName) forEach { processClass(it) }
         genContainer()
         genProperties()
         ps.close()
@@ -121,16 +117,17 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
 
     private fun genContainer() {
         ps.write(settings.getContainerHeader())
-        ps.write("\n")
+        ps.newLine()
         ps.write(containerCache.toString())
-        ps.write("}\n")
+        ps.write("}")
+        ps.newLine()
     }
 
     private fun genProperties() {
-        propMap.values().map {
+        propMap.values().forEach {
             if(it.getter != null) {
                 propsCache append if (it.setter == null) "val " else "var "
-                propsCache append it.classname
+                propsCache append it.className
                 propsCache append '.'
                 propsCache append decapitalize(it.propName)
                 propsCache append ": "
@@ -152,32 +149,32 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
         ps.write(propsCache.toString())
     }
 
-    private fun genSetter(classname: String, propName: String, setter: String, arg: Type) {
-        if(!propMap.containsKey(classname + propName)) {
-            propMap.put(classname + propName, PropData(classname, propName, null, null, setter, arg))
+    private fun genSetter(className: String, propName: String, setter: String, arg: Type) {
+        if(!propMap.containsKey(className + propName)) {
+            propMap[className + propName] = PropData(className, propName, null, null, setter, arg)
         } else {
-            val prop = propMap.get(classname + propName)
+            val prop = propMap[className + propName]
             prop!!.setter = setter
             prop.arg = arg
         }
     }
 
-    private fun genGetter(classname: String, propName: String, getter: String, propType: Type?) {
+    private fun genGetter(className: String, propName: String, getter: String, propType: Type?) {
 
-        if(!propMap.containsKey(classname + propName)) {
-            propMap.put(classname + propName, PropData(classname, propName, propType, getter, null, null))
+        if(!propMap.containsKey(className + propName)) {
+            propMap[className + propName] = PropData(className, propName, propType, getter, null, null)
         } else {
-            val prop = propMap.get(classname + propName)
+            val prop = propMap[className + propName]
             prop!!.getter = getter
             prop.propType = propType
         }
     }
 
-    private fun genContainerFun(classname: String) {
+    private fun genContainerFun(className: String) {
         containerCache append "fun "
-        containerCache append decapitalize(classname)
-        containerCache append "( init: " + classname + ".() -> Unit): " + classname + " {\n"
-        containerCache append "val v = " + classname + "(ctx)\n"
+        containerCache append decapitalize(className)
+        containerCache append "( init: " + className + ".() -> Unit): " + className + " {\n"
+        containerCache append "val v = " + className + "(ctx)\n"
         containerCache append "v.init()\n"
         containerCache append "vg.addView(v)\n"
         containerCache append "_style(v)\n"
@@ -197,31 +194,30 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
     private fun extractClasses(jarPath: String, packageName: String): ArrayList<InputStream?> {
         val packageName = packageName.replace('.', '/')
         val jarFile = JarFile(jarPath)
-        val wtf: Enumeration<JarEntry> = jarFile.entries() as Enumeration<JarEntry>
-        var entries: jet.List<JarEntry> = Collections.list(wtf)
+        var entries: jet.List<JarEntry> = Collections.list(jarFile.entries() as Enumeration<JarEntry>)
         entries = entries.filter { it.getName().startsWith(packageName) && it.getName().endsWith(".class") }
         var ret = arrayListOf<InputStream?>()
         return entries.mapTo(ret) { jarFile.getInputStream(it) }
     }
 
     inner class AdslVisitor(api: Int) : ClassVisitor(api) {
-        var classname: String = ""
+        var className: String = ""
         override fun visit(version: Int, access: Int, name: String?, signature: String?, supername: String?, p5: Array<out String>?) {
-            classname = name!!.replace('/', '.').replace('$', '.')
+            className = cleanInternalName(name!!)
             if(((access and Opcodes.ACC_ABSTRACT) != 0)) {
                 return
             }
-            if (!name!!.contains("$")) {
+            if (!name.contains("$")) {
                 genContainerFun(name.substring(name.lastIndexOf('/') + 1))
             }
         }
         override fun visitMethod(access: Int, name: String?, desc: String?, signature: String?, exceptions: Array<out String>?): MethodVisitor? {
             if((access and Opcodes.ACC_PROTECTED) != 0) return null
-            if(classname.contains("Adapter")) return null //HACK
+            if(className.contains("Adapter")) return null //HACK
             if(name != null && name.startsWith("set") && Type.getArgumentTypes(desc)?.size == 1) {
-                genSetter(classname, name.substring(3), name, Type.getArgumentTypes(desc)!![0])
+                genSetter(className, name.substring(3), name, Type.getArgumentTypes(desc)!![0])
             } else if (name != null && name.startsWith("get") && Type.getArgumentTypes(desc)?.size == 0) {
-                genGetter(classname, name.substring(3), name, Type.getReturnType(desc))
+                genGetter(className, name.substring(3), name, Type.getReturnType(desc))
             }
             return null
         }
