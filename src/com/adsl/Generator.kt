e928ba67db20b06f2b4968a79dd90561022c2f27
dedoz <d3d0zz@gmail.com>
2013-11-24 23:11:17 +0400

    add blacklists, fixes, ide files
    - add class and property blacklists, see *_blacklist.txt files
    - fix property getter generated twice, screwing up return type
    - optimize map access
    - add ide files
    - refactor some often used code, when expressions

--- src/com/adsl/Generator.kt
+++ src/com/adsl/Generator.kt
@@ -16,14 +16,17 @@ import java.nio.ByteBuffer
 import java.io.BufferedWriter
 import java.io.OutputStreamWriter
 import java.util.TreeMap
+import java.util.HashSet
+
+class IgnoredClassEx : Exception()
 
 fun typeFilter(str: String): String {
-    when (str) {
-        "java.lang.CharSequence" -> return "jet.CharSequence"
-        "java.lang.String" -> return "jet.String"
-        "java.lang.Integer" -> return "jet.Int"
-        "java.lang.Object" -> return "jet.Any"
-        else -> return str
+    return when (str) {
+        "java.lang.CharSequence" -> "jet.CharSequence"
+        "java.lang.String" -> "jet.String"
+        "java.lang.Integer" -> "jet.Int"
+        "java.lang.Object" -> "jet.Any"
+        else -> str
     }
 }
 
@@ -31,22 +34,26 @@ fun cleanInternalName(name: String): String {
     return name.replace('/', '.').replace('$', '.')
 }
 
+fun isInnerClass(name: String): Boolean {
+    return name.contains("$")
+}
+
 fun typeStr(_type: Type?, nullable: Boolean = true): String {
-    if(_type != null) {
-        when (_type.getSort()) {
-            Type.BOOLEAN -> return "Boolean"
-            Type.INT -> return "Int"
-            Type.FLOAT -> return "Float"
-            Type.DOUBLE -> return "Double"
-            Type.LONG -> return "Long"
+    if (_type != null) {
+        return when (_type.getSort()) {
+            Type.BOOLEAN -> "Boolean"
+            Type.INT -> "Int"
+            Type.FLOAT -> "Float"
+            Type.DOUBLE -> "Double"
+            Type.LONG -> "Long"
             Type.ARRAY -> {
                 when (_type.getElementType()?.getSort()) {
-                    Type.INT -> return "IntArray?"
-                    Type.FLOAT -> return "FloatArray?"
-                    Type.DOUBLE -> return "DoubleArray?"
-                    Type.LONG -> return "LongArray?"
+                    Type.INT -> "IntArray?"
+                    Type.FLOAT -> "FloatArray?"
+                    Type.DOUBLE -> "DoubleArray?"
+                    Type.LONG -> "LongArray?"
                     else -> {
-                        return "Array<" + typeFilter(typeStr(_type.getElementType(), nullable = false)) + ">?"
+                        "Array<" + typeFilter(typeStr(_type.getElementType(), nullable = false)) + ">?"
                     }
                 }
             }
@@ -63,7 +70,9 @@ fun decapitalize(str: String): String {
     return str.substring(0, 1).toLowerCase() + str.substring(1)
 }
 
-
+fun stripClassName(className: String): String {
+    return className.substring(className.lastIndexOf('.') + 1)
+}
 
 class GeneratorSettings() {
 
@@ -83,6 +92,16 @@ class GeneratorSettings() {
     fun getContainerHeader(): String {
         return readFile("cont_header.txt")
     }
+
+    fun getBlackListedClasses(): Set<String> {
+        val lines = Files.readAllLines(Paths.get("class_blacklist.txt")!!, StandardCharsets.UTF_8)
+        return HashSet<String>(lines)
+    }
+
+    fun getBlackListedProperties(): Set<String> {
+        val lines = Files.readAllLines(Paths.get("prop_blacklist.txt")!!, StandardCharsets.UTF_8)
+        return HashSet<String>(lines)
+    }
 }
 
 class Generator(val out: OutputStream, val jarPath: String, val packageName: String) {
@@ -95,17 +114,23 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
     class PropData(val className: String, val propName: String, var propType: Type?, var getter: String?, var setter: String?, var arg: Type?)
     val propMap = TreeMap<String, PropData>()
 
+    val classBlackList = settings.getBlackListedClasses()
+    val propBlackList = settings.getBlackListedProperties()
+
+    private fun isBlacklistedClass(className: String): Boolean {
+        return classBlackList contains className
+    }
+
+    private fun isBlacklistedProperty(propertyName: String): Boolean {
+        return propBlackList contains propertyName
+    }
+
     public fun run() {
         genHeader()
         extractClasses(jarPath, packageName) forEach { processClass(it) }
         genContainer()
         genProperties()
         ps.close()
-    }
-
-
-    private fun write(str: String) {
-        ps.write(str)
     }
 
     private fun genHeader() {
@@ -125,23 +150,25 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
 
     private fun genProperties() {
         propMap.values().forEach {
-            if(it.getter != null) {
-                propsCache append if (it.setter == null) "val " else "var "
-                propsCache append it.className
-                propsCache append '.'
-                propsCache append decapitalize(it.propName)
-                propsCache append ": "
-                propsCache append typeStr(it.propType)
-                propsCache append '\n'
-                propsCache append "\tget() = this."
-                propsCache append it.getter
-                propsCache append "()\n"
-                if(it.setter != null) {
-                    propsCache append "\tset(value) = "
-                    propsCache append it.setter
-                    propsCache append "(value) \n\n"
-                } else {
-                    propsCache append "\n"
+            if(!isBlacklistedProperty(it.className + '.' + decapitalize(it.propName))) {
+                if (it.getter != null) {
+                    propsCache append if (it.setter == null) "val " else "var "
+                    propsCache append it.className
+                    propsCache append '.'
+                    propsCache append decapitalize(it.propName)
+                    propsCache append ": "
+                    propsCache append typeStr(it.propType)
+                    propsCache append '\n'
+                    propsCache append "\tget() = this."
+                    propsCache append it.getter
+                    propsCache append "()\n"
+                    if (it.setter != null) {
+                        propsCache append "\tset(value) = "
+                        propsCache append it.setter
+                        propsCache append "(value) \n\n"
+                    } else {
+                        propsCache append "\n"
+                    }
                 }
             }
         }
@@ -150,29 +177,30 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
     }
 
     private fun genSetter(className: String, propName: String, setter: String, arg: Type) {
-        if(!propMap.containsKey(className + propName)) {
-            propMap[className + propName] = PropData(className, propName, null, null, setter, arg)
-        } else {
-            val prop = propMap[className + propName]
-            prop!!.setter = setter
+        val prop = propMap[className + propName]
+        if (prop != null) {
+            prop.setter = setter
             prop.arg = arg
+        } else {
+            propMap[className + propName] = PropData(className, propName, null, null, setter, arg)
         }
     }
 
     private fun genGetter(className: String, propName: String, getter: String, propType: Type?) {
-
-        if(!propMap.containsKey(className + propName)) {
-            propMap[className + propName] = PropData(className, propName, propType, getter, null, null)
-        } else {
-            val prop = propMap[className + propName]
-            prop!!.getter = getter
+        val prop = propMap[className + propName]
+        if (prop != null) {
+            if (prop.getter != null)
+                return
+            prop.getter = getter
             prop.propType = propType
+        } else {
+            propMap[className + propName] = PropData(className, propName, propType, getter, null, null)
         }
     }
 
     private fun genContainerFun(className: String) {
         containerCache append "fun "
-        containerCache append decapitalize(className)
+        containerCache append decapitalize(stripClassName(className))
         containerCache append "( init: " + className + ".() -> Unit): " + className + " {\n"
         containerCache append "val v = " + className + "(ctx)\n"
         containerCache append "v.init()\n"
@@ -181,11 +209,14 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
         containerCache append "return v\n}\n\n"
     }
 
+
     private fun processClass(classData: InputStream?) {
         val cn = AdslVisitor(Opcodes.ASM5)
         try {
             val cr = ClassReader(classData)
             cr.accept(cn, 0)
+        } catch (e: IgnoredClassEx) {
+            //optionally log something here
         } finally {
             classData?.close()
         }
@@ -204,17 +235,17 @@ class Generator(val out: OutputStream, val jarPath: String, val packageName: Str
         var className: String = ""
         override fun visit(version: Int, access: Int, name: String?, signature: String?, supername: String?, p5: Array<out String>?) {
             className = cleanInternalName(name!!)
-            if(((access and Opcodes.ACC_ABSTRACT) != 0)) {
+            if (isBlacklistedClass(className))
+                throw IgnoredClassEx()
+            if ((access and Opcodes.ACC_ABSTRACT) != 0)
                 return
-            }
-            if (!name.contains("$")) {
-                genContainerFun(name.substring(name.lastIndexOf('/') + 1))
-            }
+            if (!isInnerClass(name))
+                genContainerFun(className)
         }
         override fun visitMethod(access: Int, name: String?, desc: String?, signature: String?, exceptions: Array<out String>?): MethodVisitor? {
-            if((access and Opcodes.ACC_PROTECTED) != 0) return null
-            if(className.contains("Adapter")) return null //HACK
-            if(name != null && name.startsWith("set") && Type.getArgumentTypes(desc)?.size == 1) {
+            if ((access and Opcodes.ACC_PROTECTED) != 0)
+                return null
+            if (name != null && name.startsWith("set") && Type.getArgumentTypes(desc)?.size == 1) {
                 genSetter(className, name.substring(3), name, Type.getArgumentTypes(desc)!![0])
             } else if (name != null && name.startsWith("get") && Type.getArgumentTypes(desc)?.size == 0) {
                 genGetter(className, name.substring(3), name, Type.getReturnType(desc))
