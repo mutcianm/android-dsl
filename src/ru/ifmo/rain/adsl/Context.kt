3989397735ef843de7cc64937c5995704cf715b7
dedoz <d3d0zz@gmail.com>
2014-03-04 18:18:28 +0400

    add context tree representation
    - represent contexts as trees to better manage nested ast-like structures
    - fork context to get a new one
    - absorbChildren to squash context tree into single buffer

--- src/ru/ifmo/rain/adsl/Context.kt
+++ src/ru/ifmo/rain/adsl/Context.kt
@@ -1,31 +1,24 @@
 package ru.ifmo.rain.adsl
 
+import java.util.ArrayList
+
 open class InvalidIndent(num: Int) : DSLWriterException("Indentation level < 0: $num")
 
-class Context(val buffer: StringBuffer, var indentDepth: Int = 0) {
+class Context(val buffer: StringBuffer = StringBuffer(), var indentDepth: Int = 0) {
     val indentUnit = "    "
     private var currentIndent = indentUnit.repeat(indentDepth)
+    val children = ArrayList<Context>()
 
-    public fun inc(): Context {
+    public fun incIndent() {
         indentDepth++
         currentIndent += indentUnit
-        return this
     }
 
-    public fun dec(): Context {
+    public fun decIndent() {
         indentDepth--
         if(indentDepth < 0)
             throw InvalidIndent(indentDepth)
         currentIndent = currentIndent.substring(0, currentIndent.length - indentUnit.length)
-        return this
-    }
-
-    public fun incIndent() {
-        inc()
-    }
-
-    public fun decIndent() {
-        dec()
     }
 
     public fun write(what: String) {
@@ -49,6 +42,26 @@ class Context(val buffer: StringBuffer, var indentDepth: Int = 0) {
 
     public fun trim(num: Int) {
         buffer.delete(buffer.length-num, buffer.length)
+    }
+
+    public fun fork(newBuffer: StringBuffer = StringBuffer(), newIndentDepth: Int = indentDepth): Context {
+        val child = Context(newBuffer, newIndentDepth)
+        children.add(child)
+        return child
+    }
+
+    public fun absorbChildren(noIndent: Boolean = true) {
+        for (child in children) {
+            child.absorbChildren()
+            if (noIndent)
+                writeNoIndent(child.toString())
+            else
+                write(child.toString())
+        }
+    }
+
+    public fun toString(): String {
+        return buffer.toString()
     }
 }
 
